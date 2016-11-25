841745db66d42f01517d8354266c392c62485534
bennettm1997 <Bennett@DESKTOP-QGKDFJD.MainRouterBBR>
2016-11-24 20:49:24 -0700

    .

--- WeatherStation/spi_Module.c
+++ WeatherStation/spi_Module.c
@@ -8,10 +8,10 @@
 //pin 6.0 state 1 for high 0 for low
 int slaveSelect1(int state){
 	P6DIR |= BIT0;
-	if(state = HIGH){
+	if(state == HIGH){
 		P6OUT |= BIT0;
 	}
-	else if(state = LOW){
+	else if(state == LOW){
 		P6OUT &= ~BIT0;
 	}
 }
@@ -19,10 +19,10 @@ int slaveSelect1(int state){
 //pin 3.2 state 1 for high 0 for low
 int slaveSelect2(int state){
 	P3DIR |= BIT2;
-	if(state = HIGH){
+	if(state == HIGH){
 		P3OUT |= BIT2;
 	}
-	else if(state = LOW){
+	else if(state == LOW){
 		P3OUT &= ~BIT2;
 	}
 }
@@ -30,21 +30,34 @@ int slaveSelect2(int state){
 //pin 3.3 state 1 for high 0 for low
 int slaveSelect3(int state){
 	P3DIR |= BIT3;
-	if(state = HIGH){
+	if(state == HIGH){
 		P3OUT |= BIT3;
 	}
-	else if(state = LOW){
+	else if(state == LOW){
 		P3OUT &= ~BIT3;
 	}
 }
 
+//this is what will recieve the data
+//pin 4.1 input
+//check the voltage on the input pins (might not be high enough) if it is not reading values
 int MISO(){
-
+	P4OUT &= ~BIT1; //set to input
+	
+	//This is the input data
+	//You will have to figure out how the data is sent across the IC's and what they mean. LSB first or MSB..
+	
+	
+	
+	
 }
 int MOSI(){
 
 }
 int configure_SPI_Clock(){
-
+//oscillate the SPI CLOCK PIN at the frequency that is decided upon (1kHZ)
+//create a new timer at 1khz
+//add the timer interrupt handler -- this will actually flip the pin
 }
+
 

864911e15e5ccf883671ce28d4318bf5dae709a3
bennettm1997 <Bennett@10.201.25.63>
2016-11-28 14:50:26 -0700

    getData Updated

--- WeatherStation/get_Data.c
+++ WeatherStation/get_Data.c
@@ -16,9 +16,10 @@ active high and some are active low.
 	Performs math to convert its ADC value to a temperature
 	Returns the temperature in the form of an integer
 */
+//Wake up Slave Select for this IC; output on a pin connected to the slave select.
 int get_Temperature(){
 	sendLog("Sampling Temperature Data");
-	//Wake up Slave Select for this IC; output on a pin connected to the slave select.
+
 
 	return 10;//change to temp and add return -1 for error
 }
@@ -30,11 +31,14 @@ int get_Barometric_Pressure(void){
 	sendLog("Sampling Barometric Pressure Data");
 
 }
-//Tell the MSP when to sample temp, pressure, etc. This will just call the get functions at the
-//specific time intervals.
+
+
+/*
+ * Tell the MSP when to sample temp, pressure, etc. This will just call the get functions at the specific time intervals.
+ * Creates a new timer. Use as many capture compare registers as needed. We do not need seperate instances
+ * of timers, just different Capture compare values.
+*/
 int get_All_Data_Fast(void){
-//creates a new timer. Use as many capture compare registers as needed. We do not need seperate instances
-	//of timers, just different Capture compare values.
 
 }
 

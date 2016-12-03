dfe1bd337663edf182c0fa4c9cd1852e86686cae
berkt <berkt@10.202.152.250>
2016-12-03 15:53:32 -0700

    created functions for get data

--- WeatherStation/get_Data.c
+++ WeatherStation/get_Data.c
@@ -5,9 +5,9 @@
 CHECK THE DATA SHEETS TO MAKE SURE WE ARE SENDING THE CORRECT SIGNALS, Some of the IC's pins are
 active high and some are active low.
 */
-
-
-
+	slaveSelect1(1); //sets slave select pin to high
+	slaveSelect2(1);
+	slaveSelect3(1);
 /*
 	Tells the temperature sensor to send temperature data
 	Recieves the temperature data
@@ -15,19 +15,31 @@ active high and some are active low.
 	Returns the temperature in the form of an integer
 */
 //Wake up Slave Select for this IC; output on a pin connected to the slave select.
-uint8_t get_Temperature(){
+void get_Temperature(){
 	sendLog("Sampling Temperature Data");
-	return 10;//change to temp and add return -1 for error
+	slaveSelect1(0); //sets slave select pin to low
+	MISO();
+	slaveSelect1(1);
 }
 /*
 Wake the Barometer IC up and get a data sample. Perform the math to convert the ADC data to an integer
 representation of the barometric pressure.
 */ 
-uint8_t get_Barometric_Pressure(void){
+void get_Barometric_Pressure(void){
 	sendLog("Sampling Barometric Pressure Data");
-	return 0;
+	slaveSelect2(0); //sets slave select pin to low
+	MISO();
+	slaveSelect2(1);
+
 }
 
+void get_Humidity(){
+	sendLog("Sampling humidity Data");
+		slaveSelect2(0); //sets slave select pin to low
+		MISO();
+		slaveSelect2(1);
+
+}
 
 /*
  * Tell the MSP when to sample temp, pressure, etc. This will just call the get functions at the specific time intervals.

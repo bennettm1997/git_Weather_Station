6d7b1e192af46d7a773fdef65acdc34c05a7dcbb
bennettm1997 <Bennett@10.201.26.111>
2016-12-04 21:12:08 -0700

    Read the todo link

--- WeatherStation/packetizeData.c
+++ WeatherStation/packetizeData.c
@@ -58,7 +58,6 @@ int isFull(Weather_Packet *w_Packet){
 //possibly add and identifier letting the computer know what kind of data it is. If the serial data has a * infront, then it is this kind of data.
 int sendAPacket(Weather_Packet * w_Packet){
 	sendLog("Sending Weather Packet");
-
 	char *ITOA_PTR;
 	if((w_Packet-> barometric_Pressure) != 0 && (w_Packet-> altitude) != 0 && (w_Packet-> daylight_Level) != 0){//check if all values are not 0 inside w_Packet{
 		uart_putchar('*');

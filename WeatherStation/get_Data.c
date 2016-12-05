ff82cf3f29211d897efa368c118d48694f2ee92b
bennettm1997 <Bennett@10.201.26.111>
2016-12-04 17:41:31 -0700

    Update

---
+++ WeatherStation/get_Data.c
@@ -0,0 +1,45 @@
+#include "get_Data.h"
+#include "spi_Module.h"
+#include "send_Log.h"
+/*
+CHECK THE DATA SHEETS TO MAKE SURE WE ARE SENDING THE CORRECT SIGNALS, Some of the IC's pins are
+active high and some are active low.
+*/
+
+
+
+/*
+	Tells the temperature sensor to send temperature data
+	Recieves the temperature data
+	Performs math to convert its ADC value to a temperature
+	Returns the temperature in the form of an integer
+*/
+//Wake up Slave Select for this IC; output on a pin connected to the slave select.
+void get_Temperature(){
+	sendLog("Sampling Temperature Data");
+	slaveSelect1(0);
+	slaveSelect1(0); //sets slave selet
+}
+/*
+Wake the Barometer IC up and get a data sample. Perform the math to convert the ADC data to an integer
+representation of the barometric pressure.
+*/
+uint8_t get_Barometric_Pressure(void){
+	sendLog("Sampling Barometric Pressure Data");
+	return 0;
+}
+
+/*
+ * Tell the MSP when to sample temp, pressure, etc. This will just call the get functions at the specific time intervals.
+ * Creates a new timer. Use as many capture compare registers as needed. We do not need seperate instances
+ * of timers, just different Capture compare values.
+*/
+void get_All_Data_Fast(void){
+
+}
+
+//Tells the MSP when to sample data at specific time intervals. This will take data at slower intervals.
+void get_All_Data_Slow(void){
+
+}
+
